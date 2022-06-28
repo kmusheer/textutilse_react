9fa5f1a65c979a00574e3b42a3610afb8428482c
kmusheer <musheerk799@gmail.com>
2022-06-28 08:26:25 +0530

    Updates

---
+++ precache-manifest.4329203c823cdf47bc38b103df56675a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7937274c9140f370ac8c",
+    "url": "/textutilse/static/css/main.16166681.chunk.css"
+  },
+  {
+    "revision": "7937274c9140f370ac8c",
+    "url": "/textutilse/static/js/main.7937274c.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/textutilse/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/textutilse/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "6d4abba8d4468f1038a5",
+    "url": "/textutilse/static/js/runtime~main.6d4abba8.js"
+  },
+  {
+    "revision": "9ea579b55c9d3f0acceeea4aff222d82",
+    "url": "/textutilse/index.html"
+  }
+];
